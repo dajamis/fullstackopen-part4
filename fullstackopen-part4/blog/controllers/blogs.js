cb345f0e0afe835417cfafbc540c16618fb28583
dajamis <wichmannjanis@gmail.com>
2024-07-16 16:17:22 +0300

    exercises 4.1 - 4.19

--- fullstackopen-part4/blog/controllers/blogs.js
+++ fullstackopen-part4/blog/controllers/blogs.js
@@ -1,11 +1,24 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
-    const user = await User.findOne()
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     body.user = user._id
 
